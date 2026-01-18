24ac02badb1065f994761becb898f3dc42c1917c
ALEKHYA SARKAR <alekhya.s201@gmail.com>
2026-01-19 01:50:37 +0530

    Fixed the issue- Uploaded resume not getting removed after clicking the 'Cross' icon

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -27,6 +27,10 @@ const FileUploader = ({onFileSelect}:FileUploaderProps) => {
         maxSize: 20*1024*1024,
     })
 
+    const handleRemoveFile = () => {
+        setSelectedFile(null);
+        onFileSelect?.(null);
+    };
 
     const file = acceptedFiles[0] || null;
 
@@ -36,37 +40,38 @@ const FileUploader = ({onFileSelect}:FileUploaderProps) => {
                 <input {...getInputProps()} />
 
                 <div className="space-y-4 cursor-pointer">
-
-                    {file ? (
-                        <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                    {selectedFile ? (
+                        <div
+                            className="uploader-selected-file"
+                            onClick={(e) => e.stopPropagation()}
+                        >
                             <img src="/image/pdf.png" alt="pdf" className="size-10" />
                             <div className="flex items-center space-x-3">
                                 <div>
                                     <p className="text-sm font-medium text-gray-700 truncate max-w-xs">
-                                        {file.name}
+                                        {selectedFile.name}
                                     </p>
                                     <p className="text-sm text-gray-500">
-                                        {formatSize(file.size)}
+                                        {formatSize(selectedFile.size)}
                                     </p>
                                 </div>
                             </div>
-                            <button className="p-2 cursor-pointer" onClick={(e) => {
-                                onFileSelect?.(null)
-                            }}>
+                            <button className="p-2 cursor-pointer" onClick={handleRemoveFile}>
                                 <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
                             </button>
                         </div>
-                    ):(
+                    ) : (
                         <div>
-                            <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                                <img src="/icons/info.svg" alt="upload" className="size-20 " />
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img src="/icons/info.svg" alt="upload" className="size-20" />
                             </div>
-                            <p className="text-lg text-gray-500 ">
-                                <span className="font-semibold">
-                                Click to upload
-                                </span> or drag and drop your resume
+                            <p className="text-lg text-gray-500">
+                                <span className="font-semibold">Click to upload</span> or drag
+                                and drop
                             </p>
-                            <p className="text-lg text-gray-500 ">PDF (max {formatSize(maxFileSize)})</p>
+                            <p className="text-lg text-gray-500">
+                                PDF (max {formatSize(maxFileSize)})
+                            </p>
                         </div>
                     )}
                 </div>
